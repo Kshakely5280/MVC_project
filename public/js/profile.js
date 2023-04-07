e59e2c94021857e6d506d544b059f4e2aac13a13
Dylan <agnello.dylan95@gmail.com>
2023-04-06 20:49:10 -0600

    adding more to the routes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,4 +1,29 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
+  
+    const name = document.querySelector('#highscore-name').value.trim();
+    const score = document.querySelector('#score-value').value.trim();
+    const createdAt = document.querySelector('#created-value').value.trim();
 
-const name = document.querySelector
+    if (name && score && createdAt) {
+      const response = await fetch(`/api/highscoreRoutes`, {
+        method: 'POST',
+        body: JSON.stringify({ name, score, createdAt }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create highscore');
+      }
+    }
+  };
+  
+
+  
+  document
+    .querySelector('.highscore-list')
+    .addEventListener('click', newFormHandler);
