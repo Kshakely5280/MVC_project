b79225020a01028e6a1d5d6b41745fcea1681780
Dylan <agnello.dylan95@gmail.com>
2023-04-12 22:15:37 -0600

    final mvp

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,26 +1,36 @@
 const router = require('express').Router();
-const {User, HighScore } = require('../../models');
+const { User, HighScore } = require('../../models');
 const withAuth = require('../../utils/auth');
+const fs = require('fs');
+
+
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const highscoreData = await HighScore.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    // const highscoreData = await HighScore.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
+
+
+
+    let dylan = await fs.readFileSync('highscore.json', 'utf8', (error, data) => {
+      error ? console.error(error) : console.log('am i getting it now mr.krabs?');
     });
-
-
+    let scores = await JSON.parse(dylan).scoresArray;
+    console.log("hi", scores);
     // Pass serialized data and session flag into template
-    res.render('profile', { 
-      highscoreData, 
-      logged_in: req.session.logged_in 
+    res.render('profile', {
+      scores,
+      user: req.session.name,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
-    res.status(500).json(err);
+    res.status(501).json(err);
   }
 });
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -5,35 +5,34 @@ const newFormHandler = async (event) => {
     const score = document.querySelector('#score-value').value.trim();
     const createdAt = document.querySelector('#created-value').value.trim();
 
-    if (name && score && createdAt) {
-      const response = await fetch(`/api/highscoreRoutes`,  {
-        method: 'POST',
-        body: JSON.stringify({ name, score, createdAt }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    // if (score) {
+    //   const response = await fetch(`/api/gamepage/scores`,  {
+    //     method: 'POST',
+    //     body: JSON.stringify(score),
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //   });
   
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create highscore');
-      }
-    }
-    const response = await fetch(`/phaserGame/index.html`,  {
-      method: 'GET',
-      body: JSON.stringify({ name, score, createdAt }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    //   if (response.ok) {
+    //     document.location.replace('/profile');
+    //   } else {
+    //     alert('Failed to create highscore');
+    //   }
+    // }
+  //   if (score){
+  //   const response = await fetch(`/api/gamepage/scores`,  {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+    
+  //   if (response.ok) {
+  //     document.location.replace('/profile');
+  //   } else {
+  //     alert('Failed to get highscore');
+  //   }
+  //   }
   };
   
-  if (name && score && createdAt) {
-
-
-
-  
-  document
-    .querySelector('.highscore-list')
-    .addEventListener('click', newFormHandler);
